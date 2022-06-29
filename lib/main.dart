7fb028202202e9fdae59911e77c96e942a3e50f6
Rawa <rawaosman134@gmail.com>
2022-06-29 17:16:09 +0300

    Button added

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,14 @@ import 'package:flutter/material.dart';
 // void main() {
 //   runApp(const MyApp());
 // }
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  MyApp({Key? key}) : super(key: key);
+  var questions = [
+    "what's your favorate color?",
+    "what's your favorate animal?",
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,7 +18,14 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("my first app"),
         ),
-        body: const Text("my cool app"),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(child: Text("Answer 1"), onPressed: null),
+            ElevatedButton(onPressed: null, child: Text('Answer 2')),
+            ElevatedButton(onPressed: null, child: Text('Answer 3'))
+          ],
+        ),
       ),
     );
   }
