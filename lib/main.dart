4526e20482bc96950102b1dd5f78867a1ec6e769
Rawa <rawaosman134@gmail.com>
2022-06-29 14:19:56 +0300

    title added

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("hello world"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("my first app"),
+        ),
+        body: const Text("my cool app"),
+      ),
     );
   }
 }
